999c7c680a490f5230332f00106c2b19a8aab79b
maehw <matthiaz.weber@gmx.de>
2020-03-23 20:48:47 +0100

    Added reading and vonversion of analog input values in mass airflow sensor code

--- software/mass-airflow-sensor/sensor-software/sensor-software.ino
+++ software/mass-airflow-sensor/sensor-software/sensor-software.ino
@@ -1,8 +1,17 @@
 #include <Wire.h>
 
 // Test setup:
-//   Arduino Nano:
-//     pin asssignment A4 (SDA), A5 (SCL)
+//   Arduino Nano pin asssignment:
+//     I2C SDA:      A4
+//     I2C SCL:      A5
+//     Analog input: A0
+
+#define ANALOG_INPUT_PIN  (A0)
+
+int g_nConversionValue = 0; // digital value for sensor's analog input
+
+float countsToMillivolts(int nCounts);
+float millivoltsToMillimetersWater(float fMillivolts);
 
 void setup()
 {
@@ -14,8 +23,23 @@ void setup()
 
 void loop()
 {
-    delay(100);
-    // TODO: read analog input to get measurement from sensor
+    // read an analog input to get measurement from sensor
+    // approx. every 250 ms (i.e. at a rate of 4 Hz)
+    // (the serial output will take some of that time + loop, conversions and interrupt overhead)
+    delay(250);
+    g_nConversionValue = analogRead(ANALOG_INPUT_PIN);
+
+    // Debug output the sensor's reading (conversion valie)
+    Serial.print("Sensor value: ");
+
+    Serial.print(g_nConversionValue); // raw reading
+    Serial.print(" counts, ");
+
+    Serial.print( countsToMillivolts(g_nConversionValue) );
+    Serial.print(" mV, ");
+
+    Serial.print( millivoltsToMillimetersWater( countsToMillivolts( g_nConversionValue ) ) );
+    Serial.println(" mmH2O");
 }
 
 void receiveEvent(int nBytes)
@@ -26,5 +50,30 @@ void receiveEvent(int nBytes)
         char cRxByte = Wire.read(); // receive byte as a character
         Serial.print(cRxByte);    // print the character
     }
-    Serial.println("");           // print the integer
+    Serial.println("");           // print a line break
 }
+
+float countsToMillivolts(int nCounts)
+{
+    // For the Arduino Uno, its a 10 bit unsgined value (i.e. a range of 0..1023).
+    // Its measurement range is from 0..5 volts.
+    // This yields a resolution between readings of: 5 volts / 1024 units or approx. 4.9 mV per LSB
+    return nCounts * 4.882813f;
+}
+
+float millivoltsToMillimetersWater(float fMillivolts)
+{
+    // for now, assume a linear transfer function of the sensor,
+    // e.g. V_out = 0.6 V/kPa * dP + 0.6 V
+    //         dP = (V_out - 0.6 V) / 0.6 V/kPa 
+    //            = (V_out - 0.6 V) * 1.667 kPa/V
+    //            = (V_out - 600 mV) * (1.667*100 mmH2O) / 1000 mV // 1 kPa ~ approx. 100 mmH2O
+
+    float fPressure = (fMillivolts - 600.0f) * 0.1666666667f;
+
+    // sanity check: no negative pressure value
+    fPressure = (fPressure < 0.0f) ? 0.0f : fPressure;
+    
+    return fPressure;
+}
+
